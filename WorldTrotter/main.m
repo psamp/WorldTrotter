ec236482b6af38c3583ea562b5c4e33e147f22c7
Princess Sampson <ps@kingonomics.com>
2016-09-12 13:06:21 -0400

    Initial Commit

---
+++ WorldTrotter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WorldTrotter
+//
+//  Created by Princess Sampson on 9/12/16.
+//  Copyright © 2016 Princess Sampson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
